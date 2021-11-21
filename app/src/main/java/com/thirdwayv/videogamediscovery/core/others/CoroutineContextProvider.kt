caa0d75b375a8e0db0f8e792d0bbb496eb0c8f24
Mohamed Mosaad <mohamed.mosaad.external@atos.net>
2021-11-21 18:49:54 +0200

    Add Singlton Annotation to CoroutineContextProvider

--- app/src/main/java/com/thirdwayv/videogamediscovery/core/others/CoroutineContextProvider.kt
+++ app/src/main/java/com/thirdwayv/videogamediscovery/core/others/CoroutineContextProvider.kt
@@ -2,8 +2,10 @@ package com.thirdwayv.videogamediscovery.core.others
 
 import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
+import javax.inject.Singleton
 import kotlin.coroutines.CoroutineContext
 
+@Singleton
 open class CoroutineContextProvider @Inject constructor() {
     open val Main: CoroutineContext by lazy { Dispatchers.Main }
     open val IO: CoroutineContext by lazy { Dispatchers.IO }
